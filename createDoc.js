dd244524f2f20456902cf555ca0710e12c61d718
M. Bilal DANACI <mbilaldnc@gmail.com>
2023-08-17 04:08:22 +0300

    Only chores left

--- createDoc.js
+++ createDoc.js
@@ -1,33 +1,457 @@
-const docs = require("@googleapis/docs");
-const util = require("util");
+const fs = require("fs");
+const docx = require("docx");
+const data = require("./data.json");
+const {
+	Document,
+	Paragraph,
+	Packer,
+	TextRun,
+	Table,
+	TableRow,
+	TableCell,
+	TextDirection,
+	Style,
+	VerticalAlign,
+	AlignmentType,
+	HeightRule,
+	WidthType,
+	OverlapType,
+	RelativeVerticalPosition,
+	RelativeHorizontalPosition,
+	TableAnchorType,
+	TableLayoutType,
+	SectionType,
+} = docx;
 
-async function main() {
-	const auth = new docs.auth.GoogleAuth({
-		keyFilename: "nucleus-hasta-sonuc-280b8124794c.json",
-		// Scopes can be specified either as an array or as a single, space-delimited string.
-		scopes: ["https://www.googleapis.com/auth/documents"],
-	});
-	const authClient = await auth.getClient();
+// Documents contain sections, you can have multiple sections per document, go here to learn more about sections
+// This simple example will only contain one section
 
-	const client = await docs.docs({
-		version: "v1",
-		auth: authClient,
-	});
+// function new MyTableCell(props = { text: "", width: 550, bold: false, textDirection: TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM }) {
+// 	return new TableCell({
+// 		...props,
+// 		children: [
+// 			new Paragraph({
+// 				children: [new TextRun({ text: props.text, bold: props.bold })],
+// 				alignment: AlignmentType.CENTER,
+// 			}),
+// 		],
+// 		width: {
+// 			size: props.width,
+// 			type: WidthType.DXA,
+// 		},
+// 		textDirection: props.textDirection === "bottomToTop" ? TextDirection.BOTTOM_TO_TOP_LEFT_TO_RIGHT : TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+// 	});
+// }
 
-	// const createResponse = await client.documents.create({
-	// 	requestBody: {
-	// 		title: "Your new document!",
-	// 	},
-	// });
-
-	// console.log(createResponse.data);
-
-	const res = await client.documents.get({
-		documentId: "1XIsnBdFMJr8ntW8NnSPO7jr5OsYZpOx0M5NKUvpnnxI",
-	});
-	console.log(util.inspect(res.data, false, 17));
-	return res.data;
+class MyTableCell extends TableCell {
+	constructor(options = {}) {
+		const { text = "", width = 550, bold = false, textDirection = TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM } = options;
+		const newProps = {
+			...options,
+			children: [
+				new Paragraph({
+					children: [new TextRun({ text: text, bold: bold })],
+					alignment: AlignmentType.CENTER,
+				}),
+			],
+			verticalAlign: VerticalAlign.CENTER,
+			textDirection: textDirection === "bottomToTop" ? TextDirection.BOTTOM_TO_TOP_LEFT_TO_RIGHT : TextDirection.LEFT_TO_RIGHT_TOP_TO_BOTTOM,
+			width: {
+				size: width,
+				type: WidthType.DXA,
+			},
+		};
+		// console.log(text, width, newProps.width);
+		super(newProps);
+	}
 }
-main().catch(console.error);
 
-//1Q4C8ES-st3KARB1m6CQ8ee9TX-BoNbJRKg8yryOEgCQ
+class MyTableRow extends TableRow {
+	constructor(options) {
+		const { height = 300, children = [], ...leftovers } = options || {};
+		super({
+			...leftovers,
+			height: {
+				value: height || 300,
+				rule: HeightRule.EXACT,
+			},
+			children: children,
+		});
+	}
+}
+
+class EmptyTableRow extends MyTableRow {
+	constructor(columnCount) {
+		super({
+			children: Array.from(Array(columnCount), () => {
+				return new MyTableCell();
+			}),
+		});
+	}
+}
+
+const sections = [];
+for (const [name, dates] of Object.entries(data)) {
+	// sort from old to new
+	const revizedDates = Object.keys(dates).sort((a, b) => {
+		const splitedA = a.split(".");
+		const aYear = Number(splitedA[2]);
+		const aMonth = Number(splitedA[1]);
+		const aDay = Number(splitedA[0]);
+		const splitedB = b.split(".");
+		const bYear = Number(splitedB[2]);
+		const bMonth = Number(splitedB[1]);
+		const bDay = Number(splitedB[0]);
+		return aYear - bYear || aMonth - bMonth || aDay - bDay;
+	});
+	// console.log(revizedDates);
+	const hemogramTable = new Table({
+		float: {
+			horizontalAnchor: TableAnchorType.TEXT,
+			verticalAnchor: TableAnchorType.TEXT,
+			overlap: OverlapType.NEVER,
+		},
+		rows: [
+			new TableRow({
+				height: {
+					value: 1300,
+					rule: HeightRule.EXACT,
+				},
+				children: [
+					new MyTableCell({ bold: true, text: "Tarih", textDirection: "bottomToTop" }),
+					new MyTableCell({ text: "HGB" }),
+					new MyTableCell({ text: "HCT" }),
+					new MyTableCell({ text: "MCV" }),
+					new MyTableCell({ text: "BK" }),
+					new MyTableCell({ text: "NÖTROFİL", textDirection: "bottomToTop" }),
+					new MyTableCell({ text: "TROMBOSİT", textDirection: "bottomToTop" }),
+					new MyTableCell({ text: "CRP" }),
+					new MyTableCell({ text: "SEDİM", textDirection: "bottomToTop" }),
+					new MyTableCell({ text: "PT" }),
+					new MyTableCell({ text: "PTT" }),
+					new MyTableCell({ text: "INR" }),
+				],
+			}),
+		]
+			.concat(
+				revizedDates.map((date) => {
+					const values = dates[date];
+					return new MyTableRow({
+						children: [
+							new MyTableCell({ text: date }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("HGB"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("HCT"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("MCV"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("WBC"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("NEU"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("PLT"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("CRP"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("Sedim"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("Protrombin Zamanı"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("APTT"))] || "" }),
+							new MyTableCell({ text: values[Object.keys(values).find((key) => key.includes("INR"))] || "" }),
+						],
+					});
+				})
+			)
+			.concat(Array.from(Array(7 - revizedDates.length), () => new EmptyTableRow(12))),
+	});
+
+	let allDateValuesCombined = {};
+	for (const date of revizedDates) {
+		Object.assign(allDateValuesCombined, dates[date]);
+	}
+
+	const elisaTable = new Table({
+		// width: {
+		// 	value: 35,
+		// 	rule: WidthType.PERCENTAGE,
+		// },
+		float: {
+			horizontalAnchor: TableAnchorType.TEXT,
+			verticalAnchor: TableAnchorType.TEXT,
+			overlap: OverlapType.NEVER,
+			// relativeHorizontalPosition: RelativeHorizontalPosition.LEFT,
+			// relativeVerticalPosition: RelativeVerticalPosition.TOP,
+			leftFromText: 200,
+		},
+		rows: [
+			new MyTableRow({
+				children: [new MyTableCell({ text: "Tarih", width: 1100 }), new MyTableCell({ width: 1000 })],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "AntiHBs", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HBs"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HBcIgG", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HBc IGG"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HBcIgM", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HBc IgM"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HBeAg", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HBeAg"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "AntiHBe", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HBe"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "AntiHCV", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HCV"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "AntiHIV", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Anti HIV"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HBV-DNA", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HBV-DNA"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HCV-RNA", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HCV-RNA"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HBV Viral Yük", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HBV Viral Yük"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HCV Viral Yük", width: 1100 }),
+					new MyTableCell({
+						width: 1000,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HCV Viral Yük"))] || "",
+					}),
+				],
+			}),
+		],
+	});
+
+	const TmAndLipidTable = new Table({
+		float: {
+			horizontalAnchor: TableAnchorType.TEXT,
+			verticalAnchor: TableAnchorType.TEXT,
+			overlap: OverlapType.NEVER,
+			// relativeHorizontalPosition: RelativeHorizontalPosition.LEFT,
+			// relativeVerticalPosition: RelativeVerticalPosition.TOP,
+			leftFromText: 100,
+		},
+		rows: [
+			new MyTableRow({
+				children: [new MyTableCell({ text: "Tarih" }), new MyTableCell({ width: 800 })],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "AFP" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Alfa-feto"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "CEA" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("CEA"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "Ca19-9" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Ca 19-9"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "Ca15-3" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Ca 15-3"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "Ca125" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Ca125"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "HDL" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("HDL"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "VLDL" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("VLDL"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "LDL" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("(LDL"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "Tot.Kolest." }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Kolesterol (Total)"))] || "",
+					}),
+				],
+			}),
+			new MyTableRow({
+				children: [
+					new MyTableCell({ text: "Trigliserit" }),
+					new MyTableCell({
+						width: 800,
+						text: allDateValuesCombined[Object.keys(allDateValuesCombined).find((key) => key.includes("Trigliserid"))] || "",
+					}),
+				],
+			}),
+		],
+	});
+	sections.push({
+		properties: {
+			page: {
+				margin: {
+					//DXA values which are 1/20th of a point which is 1/72 of an inch
+					// which means 1/1440th of an inch
+					// 567 DXA = ~1 cm
+					top: 567,
+					bottom: 567,
+					left: 567,
+					right: 567,
+				},
+			},
+			type: SectionType.NEXT_PAGE,
+		},
+		children: [
+			new Paragraph({
+				children: [new TextRun({ text: "T.C. KOCAELİ ÜNİVERSİTESİ ARAŞTIRMA VE UYGULAMA HASTANESİ", bold: true })],
+				alignment: AlignmentType.CENTER,
+				style: "11pt",
+			}),
+			new Paragraph({
+				children: [new TextRun({ text: "GASTROENTEROLOJİ YATAN HASTA LABORATUVAR BULGULARI", bold: true })],
+				alignment: AlignmentType.CENTER,
+				style: "11pt",
+			}),
+			new Paragraph({
+				children: [new TextRun({ text: "Hastanın Adı Soyadı:", bold: true }), new TextRun({ text: name })],
+				alignment: AlignmentType.LEFT,
+				style: "11pt",
+			}),
+			hemogramTable,
+			elisaTable,
+			TmAndLipidTable,
+		],
+	});
+}
+
+const doc = new Document({
+	styles: {
+		default: {
+			document: {
+				run: {
+					size: "7pt",
+					font: "Times New Roman",
+				},
+			},
+		},
+		paragraphStyles: [
+			{
+				id: "11pt",
+				name: "11pt",
+				basedOn: "Normal",
+				next: "Normal",
+				run: {
+					size: "11pt",
+				},
+			},
+		],
+	},
+	sections,
+});
+
+// Used to export the file into a .docx file
+Packer.toBuffer(doc).then((buffer) => {
+	fs.writeFileSync("My Document.docx", buffer);
+});
+
+// Done! A file called 'My Document.docx' will be in your file system.
